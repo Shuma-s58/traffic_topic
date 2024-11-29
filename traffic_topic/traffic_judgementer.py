03f7e44abb234bf3a0aa80ac70f86af0b9c0bf68
Shuma-s58 <shuma.suzuki.2021@outlook.jp>
2024-11-29 03:41:49 +0000

    add reqest_flag

--- traffic_topic/traffic_judgementer.py
+++ traffic_topic/traffic_judgementer.py
@@ -10,7 +10,8 @@ from std_srvs.srv import Trigger
 import yaml
 import time
 
-CROSSPOINT_PATH = '/root/yolov8_ws/src/traffic_topic/config/crossing_points/test.yaml'
+#CROSSPOINT_PATH = '/root/yolov8_ws/src/traffic_topic/config/crossing_points/test.yaml'
+CROSSPOINT_PATH = '/root/turtlebot3_ws/src/traffic_topic/config/crossing_points/test.yaml'
 
 class TrafficJudgmenter(Node):
     def __init__(self):
@@ -32,9 +33,6 @@ class TrafficJudgmenter(Node):
         # client
         self.client = self.create_client(Trigger, 'waypoint_manager2/next_wp')
 
-        # timer
-        #self.timer = self.create_timer(2.0, self.traffic_judgment)
-
         # 各トピックからのデータを保持する変数
         self.current_waypoint_msg = None
         self.traffic_msg = None
@@ -44,6 +42,9 @@ class TrafficJudgmenter(Node):
             self.crossing_points_data = yaml.safe_load(yml)
 
         self.crossing_points_numbers = self.crossing_points_data['crossing_point_numbers']
+
+        # reqest_flag
+        traffic_request = False
 
     def current_waypoint_callback(self, msg):
         #self.get_logger().info(f'current waypoint received: {msg.data}')
@@ -63,9 +64,11 @@ class TrafficJudgmenter(Node):
         # 両方のデータが取得されていて、特定の条件を満たしているかチェック
         if self.current_waypoint_msg is not None and self.traffic_msg is not None:
             #if self.current_waypoint_msg == 5 and "blue" in self.traffic_msg:  # 条件を定義
-            if self.current_waypoint_msg in self.crossing_points_numbers and "person" in self.traffic_msg:
-                #self.next_waypoint_service()
-
+            if self.current_waypoint_msg in self.crossing_points_numbers and "person" in self.traffic_msg and not self.traffic_request:
+                
+                # flag_on
+                self.traffic_request = True
+                time.sleep(3)
                 # サービスリクエストの送信
                 self.send_request()
                 time.sleep(2)
@@ -81,6 +84,9 @@ class TrafficJudgmenter(Node):
         # 非同期でサービスコールを実行
         future = self.client.call_async(request)
         future.add_done_callback(self.callback_response)
+
+        # flag_off
+        self.traffic_request = False
 
     def callback_response(self, future):
         try:
